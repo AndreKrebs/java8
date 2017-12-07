44f5f4d41d2ccd1683c76d76ed5430096d6e00ee
André Krebs <andre.krebssilva@gmail.com>
2017-12-07 21:33:06 -0200

    Ordenando uma lista e foreach

---
+++ src/java8/OrdenaStrings.java
@@ -0,0 +1,69 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+	public static void main(String[] args) {
+		
+		List<String> palavras = new ArrayList<>();
+		palavras.add("nome da escola");
+		palavras.add("endereço da casa");
+		palavras.add("país");
+		
+		// comparador customizado
+		Comparator<String> comparador = new OrdenaListaPorTamanho();
+		
+		// ------ ordenando com java 7
+		// apenas para ordenar	
+		Collections.sort(palavras);
+		System.out.println(palavras.toString());
+		
+		// para curtomizar a ordenação
+		Collections.sort(palavras, comparador);
+		System.out.println(palavras.toString());
+		
+		// ------ ordenando com java 8
+		// apenas usando sort
+		palavras.sort(null); // não precisa do Collections		
+		System.out.println(palavras.toString());
+		
+		// com o comparator customizado
+		palavras.sort(comparador); // não precisa do Collections		
+		System.out.println(palavras.toString());
+		
+		// ------ consumir uma string com foreach java 8
+		Consumer<String> consumidor = new ConsumidorDeStrings();
+		palavras.forEach(consumidor);
+		
+	}
+}
+
+class OrdenaListaPorTamanho implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+
+		if(s1.length() < s2.length()) {
+			return -1;
+		} else if(s1.length() > s2.length()) {
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+}
+
+class ConsumidorDeStrings implements Consumer<String> {
+
+	@Override
+	public void accept(String s) {
+		// apenas imprime
+		System.out.println(s);
+	}
+	
+}
